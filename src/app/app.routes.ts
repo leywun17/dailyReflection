ca82a2422ba9d7b65ac5332a2a37ae32e98dafe3
sazx_17 <sergiobenavides780@gmail.com>
2025-05-21 19:29:30 -0500

    rutas aseguradas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,22 +14,25 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+    loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage)
   },
   {
     path: 'register',
-    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+    loadComponent: () => import('./pages/register/register.page').then(m => m.RegisterPage)
   },
   {
     path: 'create-note',
-    loadComponent: () => import('./pages/create-note/create-note.page').then( m => m.CreateNotePage)
+    loadComponent: () => import('./pages/create-note/create-note.page').then(m => m.CreateNotePage), 
+    canActivate: [AuthGuard]
   },
   {
     path: 'user-profile',
-    loadComponent: () => import('./pages/user-profile/user-profile.page').then( m => m.UserProfilePage)
+    loadComponent: () => import('./pages/user-profile/user-profile.page').then(m => m.UserProfilePage),
+    canActivate: [AuthGuard]
   },
   {
     path: 'note/:id',
-    loadComponent: () => import('./pages/note/note.page').then(m => m.NotePage)
+    loadComponent: () => import('./pages/note/note.page').then(m => m.NotePage),
+    canActivate: [AuthGuard]
   }
 ];
